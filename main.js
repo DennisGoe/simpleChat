a9d8e0121e80fe4a201c801cd7b543e44d00880b
DennisGoe <dennis.goericke@student.reutlingen-university.de>
2019-03-21 20:27:53 +0100

    continued on design

--- main.js
+++ main.js
@@ -11,11 +11,17 @@ $(function(){
     var username = $('#username');
     var footer= $('#footer');
 
+    var testMode = false;
 
     //hide the chat so user has to enter a name first
-    messageContainer.hide();
-    footer.hide();
 
+    if(testMode){
+        loginForm.hide();
+    }else{
+        messageContainer.hide();
+        footer.hide();    
+    }
+   
 
     //aalways auto scrool to bottom of chat
     chatContent.scrollTop = chatContent.scrollHeight;
@@ -31,7 +37,8 @@ $(function(){
     //server emitts the new message
     //the content of that message is wrapped in a div and appended to the chat together with the user name
     socket.on('new message', function(data){
-        chatContent.append('<div class ="well"><strong>'+ data.username +'</strong>: '+ data.msgContent +'<div>')
+        chatContent.append('<div class ="chatMessage"><strong>'+ data.username +'</br>'+'</strong> '+ data.msgContent +'<div><hr class="line">');
+        chatContent.scrollTop(chatContent.height());
     });
 
     //this is called when the user logs in 
@@ -48,10 +55,12 @@ $(function(){
       username.val('');
     });
 
+    //this is called to show all the users
+    //the server returns a list with all the users so we can loop through it and display the names 
     socket.on('get users', function(data){
       var html ='';
       for ( i = 0; i < data.length; i++){
-        html += '<li class="list-group-item">' + data[i] +'</li>';
+        html += '<li class="userNameDiv"><strong>' + data[i] +'</strong></li>';
       }
       users.html(html);
     })
